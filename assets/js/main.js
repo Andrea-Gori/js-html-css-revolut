00fb1f784b34ffb6016167fd5d5d06bbdd12a53a
Andrea Gori <Andrea.fr.gori@gmail.com>
2021-03-13 15:35:40 +0100

    mancano gli hover sui tag a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,35 @@
-console.log('ok');
+ $(document).ready(function () {
+    appari();
+    scompari();
+    big();
+
+
+})
+
+/* Functions */
+
+
+function appari() {
+    $('.width-dropdown-menu').mouseenter(function () {
+        $(this).children('.dropdown-menu').removeClass('none')
+    })
+}
+
+function scompari() {
+  $('.width-dropdown-menu').mouseleave(function () {
+      $('.dropdown-menu').addClass('none')
+  })
+}
+
+function big() {
+  $('.width-dropdown-menu').mouseenter(function (){
+    if ($(this).children('.dropdown-menu').hasClass('big')) {
+      $(this).children('.dropdown-menu').css('left', '-120px');
+    }else {
+      $(this).children('.dropdown-menu').css('left', '-25px');
+    }
+  })
+}
+
+
+
